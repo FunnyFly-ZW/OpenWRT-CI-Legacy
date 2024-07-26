1aa5ff19e4a20095ed1e2650de9673f881679835
FunnyFly <zwchn007@gmail.com>
2024-07-26 14:14:44 +0800

    Revert to Legacy

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [[ $WRT_REPO == *"lede"* ]]; then
-	sed -i "/#src/d" ./wrt/feeds.conf.default
-	sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
+#加回部分设备支持
+if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
+	cp -rf ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
+	patch -R -p1 -d ./wrt < ./Patches/mediatek/*.patch
 
-	echo "$WRT_REPO patch has been installed!"
+	echo "$WRT_TARGET patch has been installed!"
 fi
--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#安装和更新软件包
+#更新软件包
 UPDATE_PACKAGE() {
 	local PKG_NAME=$1
 	local PKG_REPO=$2
@@ -8,40 +8,50 @@ UPDATE_PACKAGE() {
 	local PKG_SPECIAL=$4
 	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
 
-	rm -rf $(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune)
+	rm -rf $(find ../feeds/ -type d -iname "*$PKG_NAME*" -prune)
 
 	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
 
 	if [[ $PKG_SPECIAL == "pkg" ]]; then
-		cp -rf $(find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune) ./
-		rm -rf ./$REPO_NAME/
+		cp -rf $(find ./$REPO_NAME/ -type d -iname "*$PKG_NAME*" -prune) ./
+		rm -rf ./$REPO_NAME
 	elif [[ $PKG_SPECIAL == "name" ]]; then
 		mv -f $REPO_NAME $PKG_NAME
 	fi
 }
 
-#UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
-UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "$([[ $WRT_REPO == *"lede"* ]] && echo "18.06" || echo "master")"
-UPDATE_PACKAGE "design" "0x676e67/luci-theme-design" "$([[ $WRT_REPO == *"lede"* ]] && echo "main" || echo "js")"
-#UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "$([[ $WRT_REPO == *"lede"* ]] && echo "main" || echo "js")"
+#更新主题
+UPDATE_PACKAGE "design" "gngpp/luci-theme-design" "$([[ $WRT_URL == *"lede"* ]] && echo "main" || echo "js")"
+UPDATE_PACKAGE "design-config" "gngpp/luci-app-design-config" "master"
+UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "$([[ $WRT_URL == *"lede"* ]] && echo "18.06" || echo "master")"
+UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "$([[ $WRT_URL == *"lede"* ]] && echo "18.06" || echo "master")"
 
 #科学上网
-UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
+UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "luci-smartdns-dev"
+#UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main"
 UPDATE_PACKAGE "passwall-packages" "xiaorouji/openwrt-passwall-packages" "main"
-UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
+UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev"
+#UPDATE_PACKAGE "helloworld" "fw876/helloworld" "master"
+rm -rf $(find ../feeds/ -type d -iname "*helloworld*" -prune)
 
-#UPDATE_PACKAGE "advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
-#UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
+if [[ $WRT_URL == *"immortalwrt"* ]]; then
+	UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "dev"
+fi
 
 #MosDNS(sbwml)
+find ../feeds/ | grep Makefile | grep mosdns | xargs rm -f
+find ../feeds/ | grep Makefile | grep v2dat | xargs rm -f
+find ../feeds/ | grep Makefile | grep v2ray-geodata | xargs rm -f
+git clone --depth=1 --single-branch --branch "v5" https://github.com/sbwml/luci-app-mosdns.git
+git clone --depth=1 --single-branch https://github.com/sbwml/v2ray-geodata.git
+#UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5"
+#UPDATE_PACKAGE "v2ray-geodata" "sbwml/v2ray-geodata" "master"
+
+#MosDNS(QiuSimons)
 #find ../feeds/ | grep Makefile | grep mosdns | xargs rm -f
-#find ../feeds/ | grep Makefile | grep v2dat | xargs rm -f
 #find ../feeds/ | grep Makefile | grep v2ray-geodata | xargs rm -f
-#git clone --depth=1 --single-branch --branch "v5" https://github.com/sbwml/luci-app-mosdns.git
-#git clone --depth=1 --single-branch https://github.com/sbwml/v2ray-geodata.git
-UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5"
-UPDATE_PACKAGE "v2ray-geodata" "sbwml/v2ray-geodata" "master"
+#git clone --depth=1 --single-branch https://github.com/QiuSimons/openwrt-mos.git
+#UPDATE_PACKAGE "mosdns" "QiuSimons/openwrt-mos" "master"
 
 #SmartDNS
 UPDATE_PACKAGE "smartdns" "pymumu/openwrt-smartdns" "master"
@@ -54,7 +64,7 @@ UPDATE_PACKAGE "luci-app-netdata" "Jason6111/luci-app-netdata" "main"
 #git clone --depth=1 --single-branch https://github.com/Jason6111/luci-app-netdata
 
 #Poweroff
-#UPDATE_PACKAGE "luci-app-poweroff" "esirplayground/luci-app-poweroff" "master"
+UPDATE_PACKAGE "luci-app-poweroff" "esirplayground/luci-app-poweroff" "master"
 #git clone --depth=1 --single-branch https://github.com/esirplayground/luci-app-poweroff
 
 #OpenAppFilter
@@ -65,49 +75,27 @@ UPDATE_PACKAGE "OpenAppFilter" "destan19/OpenAppFilter" "master"
 UPDATE_PACKAGE "luci-app-fileassistant" "kenzok78/luci-app-fileassistant" "main"
 #git clone --depth=1 --single-branch https://github.com/kenzok78/luci-app-fileassistant
 
-if [[ $WRT_REPO != *"lede"* ]]; then
-	UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
-	UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main" "pkg"
-fi
-
-if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
-	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
-fi
-
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
-	local PKG_MARK=${2:-not}
-	local PKG_FILES=$(find ./ ../feeds/packages/ -maxdepth 3 -type f -wholename "*/$PKG_NAME/Makefile")
+	local PKG_REPO=$2
+	local PKG_FILE=$(find ../feeds/packages/*/$PKG_NAME/ -type f -name "Makefile" 2>/dev/null)
 
-	echo " "
+	if [ -f "$PKG_FILE" ]; then
+		local OLD_VER=$(grep -Po "PKG_VERSION:=\K.*" $PKG_FILE)
+		local NEW_VER=$(git ls-remote --tags --sort="version:refname" "https://github.com/$PKG_REPO.git" | tail -n 1 | sed "s/.*v//")
+		local NEW_HASH=$(curl -sfL "https://codeload.github.com/$PKG_REPO/tar.gz/v$NEW_VER" | sha256sum | cut -b -64)
 
-	if [ -z "$PKG_FILES" ]; then
-		echo "$PKG_NAME not found!"
-		return
-	fi
-
-	echo "$PKG_NAME version update has started!"
-
-	for PKG_FILE in $PKG_FILES; do
-		local PKG_REPO=$(grep -Pho 'PKG_SOURCE_URL:=https://.*github.com/\K[^/]+/[^/]+(?=.*)' $PKG_FILE | head -n 1)
-		local PKG_VER=$(curl -sL "https://api.github.com/repos/$PKG_REPO/releases" | jq -r "map(select(.prerelease|$PKG_MARK)) | first | .tag_name")
-		local NEW_VER=$(echo $PKG_VER | sed "s/.*v//g; s/_/./g")
-		local NEW_HASH=$(curl -sL "https://codeload.github.com/$PKG_REPO/tar.gz/$PKG_VER" | sha256sum | cut -b -64)
-
-		local OLD_VER=$(grep -Po "PKG_VERSION:=\K.*" "$PKG_FILE")
-
-		echo "$OLD_VER $PKG_VER $NEW_VER $NEW_HASH"
-
-		if [[ $NEW_VER =~ ^[0-9].* ]] && dpkg --compare-versions "$OLD_VER" lt "$NEW_VER"; then
-			sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$NEW_VER/g" "$PKG_FILE"
-			sed -i "s/PKG_HASH:=.*/PKG_HASH:=$NEW_HASH/g" "$PKG_FILE"
-			echo "$PKG_FILE version has been updated!"
+		if dpkg --compare-versions "$OLD_VER" lt "$NEW_VER"; then
+			sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$NEW_VER/g" $PKG_FILE
+			sed -i "s/PKG_HASH:=.*/PKG_HASH:=$NEW_HASH/g" $PKG_FILE
+			echo "$PKG_NAME ver has been updated!"
 		else
-			echo "$PKG_FILE version is already the latest!"
+			echo "$PKG_NAME ver is already the latest!"
 		fi
-	done
+	else
+		echo "$PKG_NAME is not found!"
+	fi
 }
 
-#UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
-UPDATE_VERSION "sing-box" "true"
+UPDATE_VERSION "sing-box" "SagerNet/sing-box"
